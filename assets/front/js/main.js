c997868077375d566a334093ba62843442774163
assabr <assabr.org@gmail.com>
2020-11-19 10:42:18 +0100

    search modal has been made

--- assets/front/js/main.js
+++ assets/front/js/main.js
@@ -1,69 +1,101 @@
 (() => {
-	/**
-	 * Query elements
-	 */
-	const navbar = document.querySelector('.nav'),
-		sticky = navbar.offsetTop,
-		width = 600,
-		burger = document.querySelector('.burger'),
-		menu = document.querySelector('.nav__menu'),
-		submenu = document.querySelector('.has__submenu');
+  /**
+   * Query elements
+   */
+  const navbar = document.querySelector('.nav'),
+    sticky = navbar.offsetTop,
+    width = 600,
+    burger = document.querySelector('.burger'),
+    menu = document.querySelector('.nav__menu'),
+    submenu = document.querySelector('.has__submenu'),
+    openEls = document.querySelectorAll('[data-open]'),
+    closeEls = document.querySelectorAll('[data-close]'),
+    isVisible = 'is__visible',
+    isActive = 'is__active';
 
-	/**
-	 * Adding scroll class to nav
-	 */
-	const navbarScroll = () => {
-		if (window.pageYOffset > sticky && window.innerWidth >= width) {
-			navbar.classList.add('nav_scroll');
-		} else {
-			navbar.classList.remove('nav_scroll');
-		}
-	};
+  /**
+   * Adding scroll class to nav
+   */
+  const navbarScroll = () => {
+    if (window.pageYOffset > sticky && window.innerWidth >= width) {
+      navbar.classList.add('nav_scroll');
+    } else {
+      navbar.classList.remove('nav_scroll');
+    }
+  };
 
-	/**
-	 * Menu
-	 */
-	burger.addEventListener('click', () => {
-		burger.classList.toggle('burger_active');
-		menu.classList.toggle('nav__menu_active');
-	});
+  /**
+   * Menu
+   */
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('burger_active');
+    menu.classList.toggle('nav__menu_active');
+  });
 
-	/**
-	 * Removing active classes from burger and menu
-	 * if window is more than 600px
-	 * */
-	window.addEventListener('resize', () => {
-		if (window.innerWidth > width) {
-			burger.classList.remove('burger_active');
-			menu.classList.remove('nav__menu_active');
-		}
-	});
+  /**
+   * Removing active classes from burger and menu
+   * if window is more than 600px
+   * */
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > width) {
+      burger.classList.remove('burger_active');
+      menu.classList.remove('nav__menu_active');
+    }
+  });
 
-	/**
-	 * Submenu
-	 */
-	window.addEventListener('click', (e) => {
-		let target = e.target;
+  /**
+   * Submenu
+   */
+  window.addEventListener('click', (e) => {
+    let target = e.target;
 
-		do {
-			if (target == submenu && window.innerWidth < width) {
-				submenu.classList.toggle('has__submenu_active');
-				return;
-			}
-			target = target.parentNode;
-		} while (target);
-		submenu.classList.remove('has__submenu_active');
-	});
+    do {
+      if (target == submenu && window.innerWidth < width) {
+        submenu.classList.toggle('has__submenu_active');
+        return;
+      }
+      target = target.parentNode;
+    } while (target);
+    submenu.classList.remove('has__submenu_active');
+  });
 
-	// Search in navbar
-	const searchOpen = document.querySelector('.nav__btn_search');
-	const searchForm = document.querySelector('.nav__search__form');
-	searchOpen.addEventListener('click', () => {
-		searchOpen.classList.toggle('is_active');
-		searchForm.classList.toggle('nav__search_active');
-		// return false;
-	});
+  // Search Modal
+  for (const el of openEls) {
+    el.addEventListener('click', function () {
+      const modalId = this.dataset.open;
+      document.getElementById(modalId).classList.add(isVisible);
+    });
+  }
 
-	// Init functions
-	window.onscroll = navbarScroll;
+  for (const el of closeEls) {
+    el.addEventListener('click', function () {
+      this.parentElement.parentElement.parentElement.classList.remove(
+        isVisible
+      );
+    });
+  }
+
+  // If click outside of modal
+  document.addEventListener('click', (e) => {
+    if (e.target == document.querySelector('.nav__search__modal.is__visible')) {
+      document
+        .querySelector('.nav__search__modal.is__visible')
+        .classList.remove(isVisible);
+    }
+  });
+
+  document.addEventListener('keyup', (e) => {
+    // If we press the ESC
+    if (
+      e.key == 'Escape' &&
+      document.querySelector('.nav__search__modal.is__visible')
+    ) {
+      document
+        .querySelector('.nav__search__modal.is__visible')
+        .classList.remove(isVisible);
+    }
+  });
+
+  // Init functions
+  window.onscroll = navbarScroll;
 })();
